f8ee126517bc7df8d249b723136c334abac58cf5
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2023-06-01 13:29:39 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_app_service_plan" "example" {
-  name                = "myAppServicePlan"
+  name                = "myAzureCourseAppServicePlan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   sku {
@@ -28,7 +28,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_app_service" "example" {
-  name                = "myWebApp"
+  name                = "myAzureCourseApp"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
