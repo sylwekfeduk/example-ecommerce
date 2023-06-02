11b866c0159d47848aa2cae9c0a734c5dc247237
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2023-06-02 09:00:28 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "azurerm_sql_server" "example" {
   resource_group_name          = azurerm_resource_group.example.name
   version                      = "12.0"
   administrator_login          = "qperioradmin"
-  administrator_login_password = "Qperioradmin2!"
+  administrator_login_password = "Qperior29!AzureCourse"
 }
 
 resource "azurerm_sql_database" "example" {
