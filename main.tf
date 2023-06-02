075937e4d9790d369b07be744b58921106705ba4
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2023-06-02 10:27:08 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -73,3 +73,17 @@ resource "azurerm_sql_database" "example" {
   edition             = "Basic"
 }
 
+resource "azurerm_storage_account" "example" {
+  name                     = "mystorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location = local.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+}
+
+resource "azurerm_storage_container" "example" {
+  name = "mycontainer"
+  storage_account_name = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
