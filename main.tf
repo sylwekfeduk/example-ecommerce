0332cdfc391ec9c2d60a37db4df117d21d326831
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2023-06-02 09:25:37 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -44,10 +44,6 @@ resource "azurerm_app_service" "example" {
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
-  site_config {
-    dotnet_framework_version = "v5.0"
-  }
-
   app_settings = {
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.example.instrumentation_key,
     "ConnectionStrings:DefaultConnection" = "Server=tcp:${azurerm_sql_server.example.name},1433;Initial Catalog=${azurerm_sql_database.example.name};Persist Security Info=False;User ID=${azurerm_sql_server.example.administrator_login};Password=${azurerm_sql_server.example.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
