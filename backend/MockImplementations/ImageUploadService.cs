13a6d07aee9dcc0db283306bd4de1df735452565
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2023-06-02 12:03:22 +0200

    Add files via upload

---
+++ backend/MockImplementations/ImageUploadService.cs
@@ -0,0 +1,28 @@
+using Backend.ImageUploadModule;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Backend.MockImplementations;
+
+public class ImageUploadService : IImageStorageService
+{
+    private readonly IConfiguration _config;
+
+    public ImageUploadService(IConfiguration config) {
+        _config = config;
+    }
+    public async Task<string> UploadImageAsync(Guid imageId, Stream imageStream)
+    {
+        BlobServiceClient blobServiceCLient = new BlobServiceClient(_config.GetConnectionString("storage"));
+
+        BlobContainerClient blobContainerClient = blobServiceCLient.GetBlobContainerClient("mycoursestorageaccount2");
+
+        BlobClient blobClient = blobContainerClient.GetBlobClient(imageId.ToString());
+
+        await blobClient.UploadAsync(imageStream);
+
+        string blobUrl = blobClient.Uri.ToString();
+
+        return blobUrl;
+    }
+}
