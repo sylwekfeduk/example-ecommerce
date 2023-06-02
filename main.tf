dc3c3e4184688a93620e5732856a5591754e4dfa
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2023-06-02 10:35:31 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "azurerm_sql_database" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "myazurestorageaccount2"
+  name                     = "mycoursestorageaccount2"
   resource_group_name      = azurerm_resource_group.example.name
   location = local.location
   account_tier = "Standard"
@@ -83,7 +83,7 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_storage_container" "example" {
-  name = "myazurecontainer2"
+  name = "mycoursecontainer2"
   storage_account_name = azurerm_storage_account.example.name
   container_access_type = "private"
 }
